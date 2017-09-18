d7dc9b93963d6f4f67d0a9491ec2600c8e4ee2df
Dima Malnev <Dimon13074XxXx@gmail.com>
2017-09-18 20:20:36 +0300

    первоначальная версия проекта

---
+++ HelloWorld/HelloWorld/HelloWorld.cpp
@@ -0,0 +1,15 @@
+// HelloWorld.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+#include <stdlib.h>
+#include <iostream>
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	std::cout<<"Hello World!!!\n";
+	system("pause");
+	return 0;
+}
+
